1bc480340161f3b0d1a761812e750387440ba4d5
Siddhant Baviskar <siddhant.baviskar@northumbria.ac.uk>
2016-04-30 16:13:36 +0100

    location working jquery

--- drive-safe-web-api/index.php
+++ drive-safe-web-api/index.php
@@ -8,20 +8,35 @@
         <!-- Import elements to be used -->
         <link rel="import" href="bower_components/paper-card/paper-card.html">
         <link rel="import" href="bower_components/google-map/google-map.html">
-        <link rel="import" href="bower_components/geo-location/geo-location.html">
 
         <!-- css goes here -->
         <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
-    <body onload="getLocation()">
+    <body>
         <!-- Use elements -->
         <div id="top-bar">
             <h1>Drive Safe</h1>
         </div>
         <div id="map-area">
-            <google-map id="location" latitude="{{ latitude }}" longitude="{{ longitude }}"></google-map>
+            <google-map id="map" latitude="37.790" longitude="-122.390"></google-map>
         </div>
 
     </body>
-    <script src="bower_components/jquery/dist/jquery.min.js">
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        if(navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(handleGetCurrentPosition, onError);
+
+        function handleGetCurrentPosition(location){
+            $('#map').attr('latitude', location.coords.latitude);
+            $('#map').attr('longitude', location.coords.longitude);
+        }
+
+        function onError(){
+        }
+    });
+
+
+    </script>
 </html>
