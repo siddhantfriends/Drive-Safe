fd247236c8a449ac973cb3ca704273d500139f6a
Siddhant Baviskar <siddhant.baviskar@northumbria.ac.uk>
2016-04-30 15:33:27 +0100

    location added to web api 2

--- drive-safe-web-api/index.php
+++ drive-safe-web-api/index.php
@@ -8,12 +8,20 @@
         <!-- Import elements to be used -->
         <link rel="import" href="bower_components/paper-card/paper-card.html">
         <link rel="import" href="bower_components/google-map/google-map.html">
+        <link rel="import" href="bower_components/geo-location/geo-location.html">
 
         <!-- css goes here -->
         <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
-    <body>
+    <body onload="getLocation()">
         <!-- Use elements -->
-        <google-map latitude="37.790" longitude="-122.390"></google-map>
+        <div id="top-bar">
+            <h1>Drive Safe</h1>
+        </div>
+        <div id="map-area">
+            <google-map id="location" latitude="{{ latitude }}" longitude="{{ longitude }}"></google-map>
+        </div>
+
     </body>
+    <script src="bower_components/jquery/dist/jquery.min.js">
 </html>
