d2769c5d3e58a2ceb40fa73e566b3c12d4b9bbb4
Siddhant Baviskar <siddhant.baviskar@northumbria.ac.uk>
2016-04-30 17:42:54 +0100

    location services working with accuracy fixed scrolling issue

--- drive-safe-web-api/index.php
+++ drive-safe-web-api/index.php
@@ -18,23 +18,36 @@
             <h1>Drive Safe</h1>
         </div>
         <div id="map-area">
+
             <google-map id="map" latitude="37.790" longitude="-122.390" zoom="17" >
                 <google-map-marker id="map-maker" latitude="37.77493" longitude="-122.41942" icon="images/icon.png" draggable="false"></google-map-marker>
+
             </google-map>
+
         </div>
 
     </body>
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <script>
     $(document).ready(getLocation());
+        var once = true;
 
     function getLocation() {
+
+
         if(navigator.geolocation)
-            navigator.geolocation.getCurrentPosition(handleGetCurrentPosition, onError);
+            navigator.geolocation.getCurrentPosition(handleGetCurrentPosition, onError, {'enableHighAccuracy':true,'timeout':500,'maximumAge':0});
 
         function handleGetCurrentPosition(location){
-            $('#map').attr('latitude', location.coords.latitude);
-            $('#map').attr('longitude', location.coords.longitude);
+            if (once) {
+                $('#map').attr('latitude', location.coords.latitude);
+                $('#map').attr('longitude', location.coords.longitude);
+
+                once = false;
+            }
+
+
+
 
             $('#map-maker').attr('latitude', location.coords.latitude);
             $('#map-maker').attr('longitude', location.coords.longitude);
