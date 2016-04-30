bd65cac217a5d52145132ae4e2b87f4adeaa5be8
Siddhant Baviskar <siddhant.baviskar@northumbria.ac.uk>
2016-04-30 14:30:53 +0100

    ready for deployment

--- drive-safe-web-api/index.php
+++ drive-safe-web-api/index.php
@@ -1,10 +1,19 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>Drive Safe - Home</title>
+        <!-- Adding polyfill support. -->
+        <script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+
+        <!-- Import elements to be used -->
+        <link rel="import" href="bower_components/paper-card/paper-card.html">
+        <link rel="import" href="bower_components/google-map/google-map.html">
+
+        <!-- css goes here -->
+        <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
     <body>
-        <?php
-        echo '<h1>Drive Safe</h1>';
-        ?>
+        <!-- Use elements -->
+        <google-map latitude="37.790" longitude="-122.390"></google-map>
     </body>
 </html>
